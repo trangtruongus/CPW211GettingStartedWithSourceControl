5116eddc18e35c098f20b48e0654c96aa293344e
trangtruongus <trang.truong.us@icloud.com>
2023-07-11 00:26:19 -0500

    Added while loop

--- CPW211GettingStartedWithSourceControl/CPW211GettingStartedWithSourceControl/Program.cs
+++ CPW211GettingStartedWithSourceControl/CPW211GettingStartedWithSourceControl/Program.cs
@@ -1,2 +1,10 @@
 ﻿Console.WriteLine("Hello GitHub. It's nice to meet you!");
 Console.WriteLine("I am writing this line for the next commit!");
+
+int count = 5;
+
+while (count > 0)
+{
+    Console.WriteLine("Exiting in " + count);
+    count--;
+}
